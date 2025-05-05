0e80e89c856b0af1c22e7cd20248ac3af2cb967c
Kazuki Onishi <princeontrojanhorse@gmail.com>
2025-05-05 21:17:48 +0900

    wip: builtin functions ...

---
+++ src/eval/builtin.rs
@@ -0,0 +1,5 @@
+use super::value::Value;
+
+fn map(Value::Func { args, body, env }: Value, items: Vec<Value>) -> Result<Value, String> {
+
+}
--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,3 +1,4 @@
 pub mod env;
 pub mod eval;
 pub mod value;
+pub mod builtin;
