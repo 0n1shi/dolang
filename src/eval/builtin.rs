338b77225fc7a5eaab2c4914e6d79b23fe89e7d9
Kazuki Onishi <princeontrojanhorse@gmail.com>
2025-05-13 09:31:47 +0900

    feat: add a util

--- src/eval/builtin.rs
+++ src/eval/builtin.rs
@@ -64,6 +64,11 @@ pub const BUILTIN_FUNCTIONS: &[BuiltinFunc] = &[
         func: len,
         args_len: 1,
     },
+    BuiltinFunc {
+        name: "empty",
+        func: empty,
+        args_len: 1,
+    },
 ];
 
 fn format_value(value: &Value) -> String {
@@ -202,5 +207,12 @@ pub fn len(args: Vec<Value>) -> Result<Value, String> {
         [Value::List(l)] => Ok(Value::Number(l.len() as f64)),
         [Value::String(s)] => Ok(Value::Number(s.len() as f64)),
         _ => Err("len: expected a list or string".to_string()),
+    }
+}
+pub fn empty(args: Vec<Value>) -> Result<Value, String> {
+    match args.as_slice() {
+        [Value::List(l)] => Ok(Value::Boolean(l.is_empty())),
+        [Value::String(s)] => Ok(Value::Boolean(s.is_empty())),
+        _ => Err("empty: expected a list or string".to_string()),
     }
 }
