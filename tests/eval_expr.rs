77eb5dd4dff3dd4f3fd10c4a2a1090a8362438be
Kazuki Onishi <princeontrojanhorse@gmail.com>
2025-05-20 20:27:36 +0900

    test: evaluate expression

---
+++ tests/eval_expr.rs
@@ -0,0 +1,69 @@
+use dolang::ast::Expr;
+use dolang::ast::{CompOp, FactorOp, LogicOp, TermOp, UnaryOp};
+use dolang::eval::env::Env;
+use dolang::eval::eval::eval_expr;
+use dolang::eval::value::Value;
+
+#[test]
+fn test_eval_expr() {
+    let test_cases = vec![
+        (
+            // 5 + 5 == 10 and true -> true
+            Expr::Logic {
+                left: Box::new(Expr::Comp {
+                    left: Box::new(Expr::Term {
+                        left: Box::new(Expr::Number(5.0)),
+                        op: TermOp::Plus,
+                        right: Box::new(Expr::Number(5.0)),
+                    }),
+                    op: CompOp::Equal,
+                    right: Box::new(Expr::Number(10.0)),
+                }),
+                op: LogicOp::And,
+                right: Box::new(Expr::Boolean(true)),
+            },
+            Ok(Value::Boolean(true)),
+        ),
+        (
+            // 5 == 3 -> false
+            Expr::Comp {
+                left: Box::new(Expr::Number(5.0)),
+                op: CompOp::Equal,
+                right: Box::new(Expr::Number(3.0)),
+            },
+            Ok(Value::Boolean(false)),
+        ),
+        (
+            // 5 + 3 -> 8
+            Expr::Term {
+                left: Box::new(Expr::Number(5.0)),
+                op: TermOp::Plus,
+                right: Box::new(Expr::Number(3.0)),
+            },
+            Ok(Value::Number(8.0)),
+        ),
+        (
+            // 5 * 3 -> 15
+            Expr::Factor {
+                left: Box::new(Expr::Number(5.0)),
+                op: FactorOp::Multiply,
+                right: Box::new(Expr::Number(3.0)),
+            },
+            Ok(Value::Number(15.0)),
+        ),
+        (
+            Expr::Unary {
+                op: UnaryOp::Minus,
+                right: Box::new(Expr::Number(5.0)),
+            },
+            Ok(Value::Number(-5.0)),
+        ), // -5 -> -5
+        (Expr::Number(5.0), Ok(Value::Number(5.0))), // 5 -> 5
+    ];
+
+    for (input, expected) in test_cases {
+        let mut env = Env::new(None);
+        let result = eval_expr(&input, &mut env);
+        assert_eq!(result, expected, "Failed for input: {:?}", input);
+    }
+}
